063b16fbb39640fa6ead31313adb27b22c777216
Marcelo Bauer <bauersaba@gmail.com>
2022-12-12 16:17:30 -0300

    [appblockchain]fisrt app flutter with block chain

---
+++ android/app/src/main/kotlin/com/example/app_blockchain/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_blockchain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/block.dart
@@ -0,0 +1,23 @@
+import 'dart:collection';
+import 'transaction.dart';
+
+class Block {
+  final int index;
+  final int timestamp;
+  final List<Transaction> transactions;
+  int proof;
+  final String prevHash;
+
+  Block(
+      this.index, this.timestamp, this.transactions, this.proof, this.prevHash);
+
+  Map<String, dynamic> toJson() {
+    var m = new LinkedHashMap<String, dynamic>();
+    m['index'] = index;
+    m['timestamp'] = timestamp;
+    m['transactions'] = transactions.map((t) => t.toJson()).toList();
+    m['proof'] = proof;
+    m['prevHash'] = prevHash;
+    return m;
+  }
+}
